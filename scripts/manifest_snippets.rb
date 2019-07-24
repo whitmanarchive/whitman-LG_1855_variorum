2f84785dbfcca98c639587d3e2e58844b6116012
Jessica Dussault <jdussault4@gmail.com>
2019-07-24 08:57:04 -0500

    updates snippet generation to have multi-page manifests
    this supports those images where we have a cropped version
    and then a fullsized version as well

--- scripts/manifest_snippets.rb
+++ scripts/manifest_snippets.rb
@@ -9,9 +9,9 @@ require "iiif/presentation"
 require "nokogiri"
 
 # iiif config
-iiif_path = "https://whitmanarchive.org/iiif/2"
-iiif_end = "full/full/0/default.jpg"
-iiif_thumb = "full/!150,150/0/default.jpg"
+@iiif_path = "https://whitmanarchive.org/iiif/2"
+@iiif_end = "full/full/0/default.jpg"
+@iiif_thumb = "full/!150,150/0/default.jpg"
 
 groups = {}
 
@@ -25,6 +25,39 @@ output_dir = File.join(
 )
 FileUtils.mkdir_p(output_dir)
 
+def create_canvas(image_path, id, row, crop = false)
+  # create a canvas for each snippet, for now just use first image alone
+  canvas = IIIF::Presentation::Canvas.new()
+  # TODO placeholder for image pending
+  if image_path && image_path != "Image pending"
+    item_loc = image_path.gsub("/", "%2F")
+  else
+    item_loc = "test.jpg"
+  end
+  full_url = "#{@iiif_path}/#{item_loc}/#{@iiif_end}"
+  thumb_url = "#{@iiif_path}/#{item_loc}/#{@iiif_thumb}"
+
+  canvas["@id"] = "https://whitmanarchive.org/TODO/snippets/#{id}"
+  canvas.label = row["File Label"]
+  canvas.thumbnail = thumb_url
+
+  annotation = IIIF::Presentation::Annotation.new
+  begin
+    annotation.resource = IIIF::Presentation::ImageResource.create_image_api_image_resource({
+      service_id: "#{@iiif_path}/#{item_loc}"
+    })
+  rescue => e
+    puts "Unable to add manuscript for #{item_loc}: #{e}"
+    return nil
+  end
+  annotation["on"] = "https://whitmanarchive.org/TODO/snippets/#{id}"
+  annotation["@id"] = "https://whitmanarchive.org/TODO/annotation/#{id}"
+  canvas.images << annotation
+  canvas.width = annotation.resource.width
+  canvas.height = annotation.resource.height
+  canvas
+end
+
 csv = CSV.read(csv_filepath, headers: true)
 
 csv.each do |row|
@@ -34,6 +67,9 @@ csv.each do |row|
   label = "#{row["Group Name"]} #{id}"
 
   # CREATE A MANIFEST FOR EACH ROW OF THE CSV
+  # if there is a cropped version, use that as the first canvas
+  # of the manifest, and regardless include the full sized image
+  # as a canvas in the manifest
 
   manifest = IIIF::Presentation::Manifest.new({
     "@id" => "https://cdrhmedia.unl.edu/data/whitman-variorum/output/development/manifests/snippets/#{id}.json",
@@ -46,7 +82,7 @@ csv.each do |row|
     "attribution" => "Walt Whitman or other attribution",
     "viewingDirection" => "left-to-right",
     "viewingHint" => "paged",
-    "logo" => "#{iiif_path}ppp.00271.001.jpg/#{iiif_thumb}"
+    "logo" => "#{@iiif_path}ppp.00271.001.jpg/#{@iiif_thumb}"
   })
 
   # still has to be a sequence even for one image
@@ -54,38 +90,13 @@ csv.each do |row|
     "label" => "No Label Yet"
   })
 
-  # create a canvas for each snippet, for now just use first image alone
-  canvas = IIIF::Presentation::Canvas.new()
-
-  # TODO placeholder for image pending
-  if row["File location"] && row["File location"] != "Image pending"
-    item_loc = row["File location"].gsub("/", "%2F")
-  else
-    item_loc = "test.jpg"
+  # create a canvas for the cropped version if it exists and should be included
+  if row["Cropped File location"] && !row["Cropped File location"][/[Dd]o not include/]
+    canvas = create_canvas(row["Cropped File location"], "#{id}-cropped", row, crop: true)
+    sequence_primary.canvases << canvas if canvas
   end
-  full_url = "#{iiif_path}/#{item_loc}/#{iiif_end}"
-  thumb_url = "#{iiif_path}/#{item_loc}/#{iiif_thumb}"
-
-  canvas["@id"] = "https://whitmanarchive.org/TODO/snippets/#{id}"
-  canvas.label = row["File Label"]
-  canvas.thumbnail = thumb_url
-
-  annotation = IIIF::Presentation::Annotation.new
-  begin
-    annotation.resource = IIIF::Presentation::ImageResource.create_image_api_image_resource({
-      service_id: "#{iiif_path}/#{item_loc}"
-    })
-  rescue => e
-    puts "Unable to add manuscript for #{item_loc}: #{e}"
-    next
-  end
-  annotation["on"] = "https://whitmanarchive.org/TODO/snippets/#{id}"
-  annotation["@id"] = "https://whitmanarchive.org/TODO/annotation/#{id}"
-  canvas.images << annotation
-  canvas.width = annotation.resource.width
-  canvas.height = annotation.resource.height
-
-  sequence_primary.canvases << canvas
+  canvas_uncropped = create_canvas(row["File location"], id, row)
+  sequence_primary.canvases << canvas_uncropped if canvas_uncropped
 
   manifest.sequences << sequence_primary
   # puts manifest.to_json(pretty: true)
